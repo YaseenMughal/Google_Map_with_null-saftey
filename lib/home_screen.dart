36e961451c6fd9f834cabba5460dcd0da75942c9
YaseenMughal <mughal123yaseen@gmail.com>
2023-09-08 18:33:30 +0500

    Google Map with null saftey

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,7 +15,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   // 1st step
   static final CameraPosition _kGooglePlex =
-      const CameraPosition(target: LatLng(24.98263, 67.08073), zoom: 14);
+      const CameraPosition(target: LatLng(24.9838, 67.0602), zoom: 14);
 
   // 2nd step
   Completer<GoogleMapController> _controller = Completer();
@@ -25,16 +25,43 @@ class _HomeScreenState extends State<HomeScreen> {
   final List<Marker> _list = const [
     Marker(
         markerId: MarkerId('1'),
-        position: LatLng(24.98263, 67.08073),
-        infoWindow: InfoWindow(title: "Ismail Mughal House")),
+        position: LatLng(24.9838, 67.0602),
+        infoWindow:
+            InfoWindow(title: "National Cricket Stadium, Karachi-Pakistan")),
     Marker(
         markerId: MarkerId('2'),
-        position: LatLng(24.97765, 67.08012),
-        infoWindow: InfoWindow(title: "Muhammad Mughal House")),
+        position: LatLng(31.5137, 74.3333),
+        infoWindow:
+            InfoWindow(title: "Gaddafi Cricket Stadium, Lahore-Pakistan")),
     Marker(
       markerId: MarkerId('3'),
-      position: LatLng(33.6844, 73.0479),
-      infoWindow: InfoWindow(title: "Asif Taj House"),
+      position: LatLng(23.092, 72.597),
+      infoWindow: InfoWindow(
+          title: "Narendra Modi Cricket Stadium, Ahmedabad-Gujarat-India"),
+    ),
+    Marker(
+      markerId: MarkerId('4'),
+      position: LatLng(12.978806, 77.599556),
+      infoWindow:
+          InfoWindow(title: "Chinnaswamy Cricket Stadium, Bangalore-India"),
+    ),
+    Marker(
+      markerId: MarkerId('5'),
+      position: LatLng(51.5298, 0.1722),
+      infoWindow:
+          InfoWindow(title: "Lord's Cricket Stadium, Birmingham-England"),
+    ),
+    Marker(
+      markerId: MarkerId('6'),
+      position: LatLng(52.4553, 1.9041),
+      infoWindow:
+          InfoWindow(title: "Edgbaston Cricket Stadium, Birmingham-England"),
+    ),
+    Marker(
+      markerId: MarkerId('7'),
+      position: LatLng(51.4838, 0.1150),
+      infoWindow:
+          InfoWindow(title: "The Oval Cricket Stadium, Birmingham-England"),
     ),
   ];
 
@@ -51,7 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: GoogleMap(
           markers: Set<Marker>.of(_marker),
-          mapType: MapType.satellite,
+          mapType: MapType.terrain,
           myLocationEnabled: false,
           initialCameraPosition: _kGooglePlex,
           onMapCreated: (GoogleMapController controller) {
@@ -65,7 +92,7 @@ class _HomeScreenState extends State<HomeScreen> {
           onPressed: () async {
             GoogleMapController controller = await _controller.future;
             controller.animateCamera(CameraUpdate.newCameraPosition(
-                CameraPosition(target: LatLng(33.6844, 73.0479), zoom: 14)));
+                CameraPosition(target: LatLng(51.5298, 0.1722), zoom: 14)));
           }),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
